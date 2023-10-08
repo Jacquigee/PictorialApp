f5ce4ca42facd34fe8c42d04bcdf47c9d563e20c
jacqui <gitaujaquiline@gmail.com>
2023-10-08 21:02:51 +0300

    Making the DiscoverScreen scrollable

--- app/src/main/java/com/example/pictorialapp/ui/screens/DiscoverScreen.kt
+++ app/src/main/java/com/example/pictorialapp/ui/screens/DiscoverScreen.kt
@@ -1,18 +1,11 @@
 package com.example.pictorialapp.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.pictorialapp.models.Images
 import com.example.pictorialapp.models.User
-import com.example.pictorialapp.models.photos
 import com.example.pictorialapp.models.userList
 import com.example.pictorialapp.ui.components.ProminentImageItem
 
@@ -27,7 +20,7 @@ import com.example.pictorialapp.ui.components.ProminentImageItem
 @Composable
 fun DiscoverScreen() {
     WhatsNew(userList = userList)
-    Browse(photos = photos)
+    //Browse(photos = photos)
 }
 @Composable
 fun WhatsNew(
@@ -42,22 +35,22 @@ fun WhatsNew(
     }
 }
 
-@Composable
-fun Browse(
-    modifier: Modifier = Modifier,
-    photos: List<Images>
-){
-    LazyVerticalStaggeredGrid(
-        modifier = modifier,
-        columns = StaggeredGridCells.Fixed(2),
-        verticalItemSpacing = 4.dp,
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-    ){
-        items(photos){ Images ->
-            Browse(photos = photos)
-        }
-    }
-}
+//@Composable
+//fun Browse(
+//    modifier: Modifier = Modifier,
+//    photos: List<Images>
+//){
+//    LazyVerticalStaggeredGrid(
+//        modifier = modifier,
+//        columns = StaggeredGridCells.Fixed(2),
+//        verticalItemSpacing = 4.dp,
+//        horizontalArrangement = Arrangement.spacedBy(4.dp),
+//    ){
+//        items(photos){ Images ->
+//            Browse(photos = photos)
+//        }
+//    }
+//}
 
 
 
