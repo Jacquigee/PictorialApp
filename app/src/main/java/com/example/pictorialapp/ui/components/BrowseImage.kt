24582c979092dcbd565ce6694918417ff7c660f2
jacqui <gitaujaquiline@gmail.com>
2023-10-10 06:22:14 +0300

    Added content padding in LazyRow

--- app/src/main/java/com/example/pictorialapp/ui/components/BrowseImage.kt
+++ app/src/main/java/com/example/pictorialapp/ui/components/BrowseImage.kt
@@ -1,15 +1,21 @@
 package com.example.pictorialapp.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pictorialapp.models.Images
 import com.example.pictorialapp.models.photos
-import com.example.pictorialapp.ui.screens.Browse
+
 
 /**
  * PROJECT NAME: Pictorial App
@@ -20,15 +26,37 @@ import com.example.pictorialapp.ui.screens.Browse
  */
 
 @Composable
-fun BrowseImage(modifier:Modifier = Modifier, header: String, images: Images){
-    LazyVerticalStaggeredGrid(
-        modifier = modifier,
-        columns = StaggeredGridCells.Fixed(2),
-        verticalItemSpacing = 4.dp,
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-    ){
-        items(photos){ Images ->
-            Browse(photos = photos)
+fun BrowseImage(modifier: Modifier = Modifier) {
+    Browse(photos = photos)
+}
+
+
+@Composable
+fun Browse(
+    modifier: Modifier = Modifier,
+    photos: List<Images>
+) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())) {
+        LazyVerticalStaggeredGrid(
+            modifier = modifier.weight(1f),
+            columns = StaggeredGridCells.Fixed(2),
+            verticalItemSpacing = 4.dp,
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            contentPadding = PaddingValues(10.dp)
+        ) {
+            items(photos) { Images ->
+                Browse(photos = photos)
+            }
         }
     }
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BrowsePreview() {
+    BrowseImage()
 }
--- app/src/main/java/com/example/pictorialapp/ui/screens/DiscoverScreen.kt
+++ app/src/main/java/com/example/pictorialapp/ui/screens/DiscoverScreen.kt
@@ -2,23 +2,18 @@ package com.example.pictorialapp.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.pictorialapp.models.Images
 import com.example.pictorialapp.models.User
-import com.example.pictorialapp.models.photos
 import com.example.pictorialapp.models.userList
-import com.example.pictorialapp.ui.components.BrowseImage
 import com.example.pictorialapp.ui.components.ProminentImageItem
 
 /**
@@ -38,7 +33,6 @@ fun DiscoverScreen( modifier: Modifier = Modifier) {
         .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(24.dp)) {
         WhatsNew(userList = userList)
-        Browse(photos = photos)
     }
 }
 @Composable
@@ -47,31 +41,16 @@ fun WhatsNew(
     userList: List<User>
 
     ){
-    LazyRow(modifier = modifier){
-        items(userList) {User ->
+    LazyRow(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        items(userList) { User ->
             ProminentImageItem(
                 header = "What's New Today",
                 user = User
             )
-        }
-    }
-}
-
-@Composable
-fun Browse(
-    modifier: Modifier = Modifier,
-    photos: List<Images>
-){
-    LazyVerticalStaggeredGrid(
-        modifier = modifier,
-        columns = StaggeredGridCells.Fixed(2),
-        verticalItemSpacing = 4.dp,
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-
-
-    ){
-        items(photos){ Images ->
-            BrowseImage(header = "Browse All", images = Images)
         }
     }
 }
