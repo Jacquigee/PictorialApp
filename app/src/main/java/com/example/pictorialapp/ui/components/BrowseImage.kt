33026e08ae343841ec3adf68cd25d02ff1af042e
jacqui <gitaujaquiline@gmail.com>
2023-10-12 23:19:34 +0300

    Adding Browse Image component

--- app/src/main/java/com/example/pictorialapp/ui/components/BrowseImage.kt
+++ app/src/main/java/com/example/pictorialapp/ui/components/BrowseImage.kt
@@ -1,16 +1,11 @@
 package com.example.pictorialapp.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pictorialapp.models.Images
@@ -26,37 +21,18 @@ import com.example.pictorialapp.models.photos
  */
 
 @Composable
-fun BrowseImage(modifier: Modifier = Modifier) {
-    Browse(photos = photos)
-}
-
-
-@Composable
-fun Browse(
-    modifier: Modifier = Modifier,
-    photos: List<Images>
-) {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())) {
-        LazyVerticalStaggeredGrid(
-            modifier = modifier.weight(1f),
-            columns = StaggeredGridCells.Fixed(2),
-            verticalItemSpacing = 4.dp,
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            contentPadding = PaddingValues(10.dp)
-        ) {
-            items(photos) { Images ->
-                Browse(photos = photos)
-            }
-        }
+fun BrowseImage(modifier: Modifier = Modifier, images: Images) {
+    Column(modifier = modifier) {
+        Image(
+            modifier = Modifier.size(128.dp),
+            painter = painterResource(id = images.image),
+            contentDescription = "Image" )
     }
-
 }
 
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun BrowsePreview() {
-    BrowseImage()
+    BrowseImage(images = photos.first())
 }
